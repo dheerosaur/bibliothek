d723b088dbb84834975e56f4d1d8670c7738e00c
Dheeraj Sayala <dheerosaur@gmail.com>
2013-07-16 01:43:09 +0530

    Add optional django_extensions to INSTALLED_APPS

--- bibliothek/settings.py
+++ bibliothek/settings.py
@@ -126,6 +126,12 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
 )
 
+try:
+    import django_extensions
+    INSTALLED_APPS += ('django_extensions',)
+except ImportError:
+    pass
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
