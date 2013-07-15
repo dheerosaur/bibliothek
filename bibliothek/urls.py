fc9868469c5f04af898c60202adebb6073fbe437
Dheeraj Sayala <dheerosaur@gmail.com>
2013-07-16 01:52:04 +0530

    Add home page url as a TemplateView

--- bibliothek/urls.py
+++ bibliothek/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'bibliothek.views.home', name='home'),
+
+    url(r'^$',
+        TemplateView.as_view(template_name='index.html'),
+        name='home'),
+
     # url(r'^bibliothek/', include('bibliothek.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
