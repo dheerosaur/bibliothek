b9cc719e4caa18a57fe4a54520b2c57c5101edc9
Dheeraj Sayala <dheerosaur@gmail.com>
2013-07-16 22:14:45 +0530

    Add render, deleteBook methods of BookView

--- public/js/views/book.js
+++ public/js/views/book.js
@@ -3,6 +3,10 @@ var app = app || {};
 app.BookView = Backbone.View.extend({
   tagName: 'div',
 
+  events: {
+    'click .delete': 'deleteBook'
+  },
+
   className: 'bookContainer pure-u-1-3',
 
   template: _.template( $('#bookTemplate').html() ),
@@ -11,7 +15,13 @@ app.BookView = Backbone.View.extend({
   },
 
   render: function () {
+    this.$el.html( this.template(this.model.toJSON()) );
     return this;
+  },
+
+  deleteBook: function () {
+    this.model.destroy();
+    this.remove();
   }
 
 });
