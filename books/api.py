2dbcfb6f37a27650fffe35102523a84085cbfe32
Dheeraj Sayala <dheerosaur@gmail.com>
2013-07-16 23:29:20 +0530

    Handle json serialization of keywords field

--- books/api.py
+++ books/api.py
@@ -1,4 +1,7 @@
+import json
+
 from tastypie.resources import ModelResource
+from tastypie.authorization import Authorization
 
 from .models import Book
 
@@ -7,4 +10,9 @@ class BookResource(ModelResource):
 
     class Meta:
         queryset = Book.objects.all()
+        authorization = Authorization()
         resource_name = 'books'
+
+    def dehydrate_keywords(self, bundle):
+        json_str = json.dumps(bundle.obj.keywords)
+        return json.loads(json_str)
