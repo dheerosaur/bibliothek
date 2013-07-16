7aff3e73db6438f3f7467efba008fecdb43e8fb6
Dheeraj Sayala <dheerosaur@gmail.com>
2013-07-16 22:24:03 +0530

    Initial version of Book model

--- books/models.py
+++ books/models.py
@@ -1,1 +1,16 @@
 from django.db import models
+from jsonfield import JSONField
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    releaseDate = models.DateTimeField(blank=True, null=True)
+    keywords = JSONField(default='[]')
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'book'
+        verbose_name_plural = 'books'
