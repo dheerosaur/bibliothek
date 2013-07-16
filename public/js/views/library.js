5cf24bb9be8360378c1bff17b870c6a46dec5e76
Dheeraj Sayala <dheerosaur@gmail.com>
2013-07-16 22:15:32 +0530

    Add render, addBook methods of LibraryView

--- public/js/views/library.js
+++ public/js/views/library.js
@@ -3,11 +3,46 @@ var app = app || {};
 app.LibraryView = Backbone.View.extend({
   el: '#books',
 
+  events: {
+    'click #add':   'addBook',
+  },
+
   initialize: function (initialBooks) {
+    this.$list = this.$('#book-list');
+    this.books = new app.Library();
+
+    this.listenTo(this.books, 'reset', this.render);
+    this.listenTo(this.books, 'add', this.renderBook);
+
+    this.books.reset(initialBooks);
+
   },
 
   render: function () {
+    this.books.each(function (book) {
+      this.renderBook(book);
+    }, this);
     return this;
   },
+
+  renderBook: function (book) {
+    var bookView = new app.BookView({model: book});
+    this.$list.append(bookView.render().el);
+  },
+
+  addBook: function (e) {
+    e.preventDefault();
+
+    var formData = {};
+
+    $('#addBook div').children('input').each(function (i, el) {
+      var val = $(el).val();
+      if ( val != '') {
+        formData[el.id] = val;
+      }
+    });
+
+    this.books.create(new app.Book(formData));
+  }
 
 });
